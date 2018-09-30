7bdce9b6ff085b5ec46741ecb8b6c56abfd4a803
Quantumdit <yehonatan.shabash@ucalgary.ca>
2018-09-30 00:46:34 -0600

    Started basic work
    Finished IntPair, started work on PrimeFactorization, nothing on
    SylowClassifier.

---
+++ PrimeFactorization.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class PrimeFactorization{
+	private List<IntPair> factorization = new ArrayList<IntPair>();		//First number is a prime, second number is exponent
+	
+	public PrimeFactorization(int number)
+	{
+		List<Integer> primes = new ArrayList<Integer>();			//List of primes already discovered
+		//TODO
+	}
+}
---
+++ SylowClassifier.java
@@ -0,0 +1,9 @@
+//Given a group size, returns some information about the group by applying the Sylow theorems
+//Done as a project, so I tried implementing some things on my own rather than implementing existing solutions
+//Made by Yehonatan Shabash
+
+public class SylowClassifier{
+	public static void main(String arg[]){
+		//TODO
+	}
+}
