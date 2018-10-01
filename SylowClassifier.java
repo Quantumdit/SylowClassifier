2f42dbd687132af5c6efeba3eb89c789ae099452
Quantumdit <yehonatan.shabash@ucalgary.ca>
2018-09-30 19:27:01 -0600

    Implemented user input

--- SylowClassifier.java
+++ SylowClassifier.java
@@ -2,10 +2,16 @@
 //Done as a project, so I tried implementing some things on my own rather than implementing existing solutions
 //Made by Yehonatan Shabash
 
+import java.util.Scanner;
+
 public class SylowClassifier{
 	public static void main(String arg[]){
-		LongPair[] pf = MathHandler.PrimeFactorization(988624);	//DEBUG
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Please enter the size of the group: ");
+		long input = reader.nextLong();
+		LongPair[] pf = MathHandler.PrimeFactorization(input);
 		String report = Prover.firstSylowTheorem(pf);	//DEBUG
 		System.out.println(report);			//DEBUG
+		reader.close();
 	}
 }
